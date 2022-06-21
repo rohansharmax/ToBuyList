d5985a91babafe595b9425eab7362c4bd036cd8f
rohansharmax <rohansharma22r@gmail.com>
2022-06-21 15:12:10 +0530

    final code

--- src/App.js
+++ src/App.js
@@ -1,62 +1,44 @@
-import logo from "./logo.svg";
-
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
-// import About from "./components/about";
-import React, { useState } from "react";
-import Alert from "./components/alert";
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import TodoList from "./components/TodoList";
 
-function App() {
-  const [mode, setMode] = useState("light");
-  const [alert, setAlert] = useState(null);
-  const showAlert = (message, type) => {
-    setAlert({
-      msg: message,
-      type: type,
-    });
-    setTimeout(() => {
-      setAlert(null);
-    }, 3000);
-  };
-  const toggleMode = () => {
-    if (mode === "light") {
-      setMode("dark");
-      document.body.style.backgroundColor = "#042743";
-      showAlert("Dark mode has been enabled", "success");
-      document.title = "Textify - Dark Mode";
-    } else {
-      setMode("light");
-      document.body.style.backgroundColor = "white";
-      showAlert("light mode has been enabled", "success");
-    }
-  };
+const App = () => {
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(initialState);
+  const [editTodo, setEditTodo] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   return (
-    <>
-      {/* <Router> */}
-      <Navbar title="Textify" mode={mode} toggleMode={toggleMode}></Navbar>
-      <Alert alert={alert} />
-      <div className="container my-3">
-        {
-          /* <Switch>
-            {/* <Route exact path="/about">
-              <About />
-            </Route>
-            <Route exact path="/">*/
-          <TextForm
-            showAlert={showAlert}
-            heading="Try Textify- Word Counter, Character Counter, Remove Extra Spaces and Much More | Enter Your Text Below "
-            mode={mode}
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+            editTodo={editTodo}
+            setEditTodo={setEditTodo}
           />
-        }
+        </div>
+        <div>
+          <TodoList
+            todos={todos}
+            setTodos={setTodos}
+            setEditTodo={setEditTodo}
+          />
+        </div>
       </div>
-
-      {/* </Route>  */}
-      {/* // </Switch>  */}
-      {/* </Router> */}
-    </>
+    </div>
   );
-}
+};
 
 export default App;
